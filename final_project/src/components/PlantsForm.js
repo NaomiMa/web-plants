7eda7c197282815bda5e3250f08303b4c889c5c6
NaomiMa <naomimatari93@gmail.com>
2022-04-23 19:51:56 +0300

    Adding a plant
    Adding a plant and test tags list

--- final_project/src/components/PlantsForm.js
+++ final_project/src/components/PlantsForm.js
@@ -1,32 +1,64 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase/config";
 import { useState } from "react";
+import TransferList from "./tagsList";
+import { Container } from "@mui/material";
 
 //Adding plants data in real time to firestore
 export default function PlantForm() {
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [treatment, setTreatment] = useState("")
   const [newPlant, setNewPlant] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     await addDoc(collection(db, "plants"), {
-      name: newPlant,
+      name: name,
+      description: description,
+      treatment: treatment
+     
     });
+    
     setNewPlant("");
   };
 
   return (
+    <Container>
+      <h1>הוספת צמח</h1>
     <form onSubmit={handleSubmit}>
       <label>
-        <span>Add a new book title:</span>
+          <h3>שם הצמח</h3>
         <input
           required
           type="text"
-          onChange={(e) => setNewPlant(e.target.value)}
-          value={newPlant}
-        />
-      </label>
-      <button>Add</button>
-    </form>
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          />
+        </label>
+        <label>
+          <h3>תיאור</h3>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          />
+        </label>
+        <label>
+          <h3>אופן הטיפול</h3>
+        <input
+          required
+          type="text"
+          onChange={(e) => setTreatment(e.target.value)}
+          value={treatment}
+          />
+        </label>
+        <TransferList />
+        <br/>
+      <button>הוספה</button>
+      </form>
+    </Container>
   );
 }
