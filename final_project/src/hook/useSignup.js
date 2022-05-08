52c29575336b1afb1a5190fc23d44d32a3301be0
NaomiMa <naomimatari93@gmail.com>
2022-05-08 20:40:28 +0300

    Uploading profile images and creating user doc
    Uploading profile images and creating user doc

--- final_project/src/hook/useSignup.js
+++ final_project/src/hook/useSignup.js
@@ -1,22 +1,61 @@
-import { useState } from "react";
-
-import { auth } from "../firebase/config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { auth, storage, db } from "../firebase/config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
+import { ref, getDownloadURL,  uploadBytes } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = (email, password) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((res) => {
-        dispatch({ type: "LOGIN", payload: res.user });
-      })
-      .catch((err) => {
-        setError(err.massage);
+    setIsPending(true);
+
+    try {
+      // signup
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      if (!res) {
+        throw new Error("Could not complete signup");
+      }
+      // upload user thumbnail
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      const img = await uploadBytes(ref(storage, uploadPath, thumbnail));
+      const imgUrl = await getDownloadURL(img.ref);
+
+      // add display name to user
+      await updateProfile(res.user, { displayName, photoURL: imgUrl });
+
+
+      // create a user document
+      await setDoc(doc(db, "users", res.user.uid), {
+        online: true,
+        displayName,
+        photoURL: imgUrl,
       });
+
+      // dispatch login action
+      dispatch({ type: "LOGIN", payload: res.user });
+
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
   };
-  return { error, signup };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { signup, error, isPending };
 };
--- final_project/src/pages/Signup.js
+++ final_project/src/pages/Signup.js
@@ -1,3 +1,4 @@
+import "./Signup.css";
 import { useState } from "react";
 import { useSignup } from "../hook/useSignup";
 import TextField from "@mui/material/TextField";
@@ -10,86 +11,128 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
   const [lastName, setLastName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const { signup, isPending, error  } = useSignup();
 
-  const { error, signup } = useSignup();
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError("Image file size must be less than 100kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log("thumbnail updated");
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(password == confirmPassword){
-      signup(email, password)
-    }
-    else console.log('סיסמא לא תואמת')
+    if (password == confirmPassword) {
+      signup(email, password, displayName, thumbnail);
+      console.log(email, password, displayName, thumbnail);
+    } else console.log("סיסמא לא תואמת");
   };
 
   return (
-    <div>
+    <div className="auth-form">
       <Container>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            backgroundColor: "primary",
-          }}
-        >
-          <h2>הרשמה:</h2>
-          <form onSubmit={handleSubmit}>
-            <label>
-              <Typography variant="h6" gutterBottom component="div">
-                email:
-              </Typography>
+        <h2>הרשמה:</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <Typography variant="h6" gutterBottom component="div">
+              email:
+            </Typography>
 
-              <TextField
-                required
-                id="outlined-password-input"
-                label="email"
-                type="email"
-                autoComplete="username"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </label>
-            <label>
-              <br />
-              <Typography variant="h6" gutterBottom component="div">
-                password:
-              </Typography>
-              <span></span>
-              <TextField
-                required
-                id="outlined-password-input"
-                label="password"
-                type="password"
-                autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </label>
-            <label>
-              <br />
-              <Typography variant="h6" gutterBottom component="div">
-                confirm password:
-              </Typography>
-              <span></span>
-              <TextField
-                required
-                id="outlined-password-input"
-                label="confirm-password"
-                type="password"
-                autoComplete="current-password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                value={confirmPassword}
-              />
-            </label>
-            <br /> <br />
-            {/* לבדוק למה הכפתור לא מתקשר */}
-            {/* <Button variant="outlined">Sign up</Button> */}
+            <TextField
+              required
+              id="outlined-password-input"
+              label="email"
+              type="email"
+              autoComplete="username"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <Typography variant="h6" gutterBottom component="div">
+              password:
+            </Typography>
+            <span></span>
+            <TextField
+              required
+              id="outlined-password-input"
+              label="password"
+              type="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <label>
+            <Typography variant="h6" gutterBottom component="div">
+              confirm password:
+            </Typography>
+            <span></span>
+            <TextField
+              required
+              id="outlined-password-input"
+              label="confirm-password"
+              type="password"
+              autoComplete="current-password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+            />
+          </label>
+          <label>
+            <Typography variant="h6" gutterBottom component="div">
+              שם משתמש:
+            </Typography>
+            <span></span>
+            <TextField
+              required
+              label="שם משתמש"
+              type="text"
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+            />
+          </label>
+          <label>
+            <Typography variant="h6" gutterBottom component="div">
+              תמונת פרופיל:
+            </Typography>
+            <span></span>
+            <TextField
+              required
+              label="תמונת פרופיל"
+              type="file"
+              onChange={handleFileChange}
+            />
+            {thumbnailError && <div className="error">{thumbnailError}</div>}
+          </label>
+          <br /> <br />
+          {/* לבדוק למה הכפתור לא מתקשר */}
+          {/* <Button variant="outlined">Sign up</Button> */}
+          {!isPending && <button className="btn">sign up</button>} 
+          {isPending && <button className="btn" disabled>loading</button>} 
+          {error && <div className='error'>{ error }</div>}
 
-            <button>sign up</button>
-            {error && <p> {error} </p>}
-          </form>
-        </Box>
+        </form>
       </Container>
     </div>
   );
